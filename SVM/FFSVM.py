c1359598b8227be3b0e70b7ff2cd79898de5567a
Rafiullah Khan <47861887+rafiullahkhan22@users.noreply.github.com>
2020-05-07 12:25:33 +0500

    Add files via upload

---
+++ SVM/FFSVM.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 23:53:30 2019
+
+@author: LENOVO
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset=pd.read_csv('forestfires.csv')
+
+X=dataset.iloc[:,[0,1,4,5,6,7,8,9,10,11,12]].values
+y = dataset.iloc[:, -1].values
+
+#from sklearn.preprocessing import Imputer
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#==================================================================
+import seaborn as sns
+class_names=[0,1,2] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+# create heatmap
+sns.heatmap(pd.DataFrame(cm), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+
+from sklearn import metrics
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+h = .02  # step size in the mesh
+X_train, y_train = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = classifier.predict(np.c_[X_train.ravel(), y_train.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(X_train.shape)
+plt.figure(1, figsize=(4, 3))
+plt.pcolormesh(X_train, y_train, Z, cmap=plt.cm.RdPu)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=plt.cm.RdYlBu_r)
+plt.title('SVM (Test set)')
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+
+plt.xlim(X_train.min(), X_train.max())
+plt.ylim(y_train.min(), y_train.max())
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
---
+++ SVM/LetterRecognition_SVM.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 02:39:09 2019
+
+@author: LENOVO
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 01:12:56 2019
+
+@author: LENOVO
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('letter-recognition.data')
+X = dataset.iloc[:,1:17].values
+y = dataset.iloc[:, 0].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+# Fitting K-NN to the Training setfrom sklearn.svm import SVC
+from sklearn.svm import SVC
+
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+import seaborn as sns
+class_names=[0,1] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+# create heatmap
+sns.heatmap(pd.DataFrame(cm), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+from sklearn import metrics
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
